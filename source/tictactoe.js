1a35bc958d55ebb96cacfa9ffce4c4da87d37583
Brian Ortiz <bortiznine@gmail.com>
2021-03-17 14:34:52 -0500

    TTT edit 4 added queryselector all and events for color change and consolelog

--- source/tictactoe.js
+++ source/tictactoe.js
@@ -1,6 +1,19 @@
 //Write variables for player 1/2
 
 //write functions to hold data
+ let squares = document.querySelectorAll('.gridItemContainer>div')
+// square1.addEventListener('click',(event) =>{
+//
+   console.log(squares)
+squares.forEach( square=>
+{
+    square.addEventListener('click', (event)=>{
+        event.target.setAttribute('style', 'background-color:red')
+        console.log('its been pressed',event.target)
+    })
+})
+//     }
+// )
 
 //function for mouseover
 // var gridcolorChange=document.querySelector('.item_one')
