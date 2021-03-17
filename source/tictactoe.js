caf2358b0f9e2ed673bcfc7e858f12ecc271a3f2
Brian Ortiz <bortiznine@gmail.com>
2021-03-17 11:22:35 -0500

    TTT edit 3 added custom image and hover property on squares

--- source/tictactoe.js
+++ source/tictactoe.js
@@ -2,3 +2,20 @@
 
 //write functions to hold data
 
+//function for mouseover
+// var gridcolorChange=document.querySelector('.item_one')
+//
+// gridcolorChange.addEventListener('click',func)
+// gridcolorChange.addEventListener('click',func1)
+//
+// function func()
+// {
+//
+//     gridcolorChange.style.backgroundColor= 'blue'
+//     console.log(gridcolorChange)
+// }
+//
+// function func1()
+// {
+//     gridcolorChange.setAttribute('style', 'background-color:white')
+// }
