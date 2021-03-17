f4b1b98dba3ba4382b5747ad2f12acd5cbfc9c3a
Brian Ortiz <bortiznine@gmail.com>
2021-03-17 16:25:41 -0500

    TTT edit 4 added conditons for first row winner need to add rest of conditions and adjust GUI

--- source/tictactoe.js
+++ source/tictactoe.js
@@ -1,17 +1,55 @@
 //Write variables for player 1/2
 
 //write functions to hold data
- let squares = document.querySelectorAll('.gridItemContainer>div')
-// square1.addEventListener('click',(event) =>{
-//
-   console.log(squares)
-squares.forEach( square=>
-{
-    square.addEventListener('click', (event)=>{
-        event.target.setAttribute('style', 'background-color:red')
-        console.log('its been pressed',event.target)
-    })
+
+
+
+
+const moves = new Array(9)
+
+const squares = document.querySelectorAll('#gridItemContainer > div')
+let turnCount = 0
+let turn = 'X'
+
+function checkForWin() {
+    if (moves[0] === moves[1] && moves[1] === moves[2]) {
+        removeClickEventCallback()
+        alert('winner in the first row ' + turn)
+    } else {
+        console.log('no winner yet')
+    }
+}
+
+function removeClickEventCallback() {
+    squares.forEach(square =>
+        square.removeEventListener('click', handleClickEventCallback)
+    )
+}
+
+function handleClickEventCallback(event) {
+    moves[event.target.id] = turn
+    checkForWin()
+    event.target.innerText = turn
+    turn = (turnCount % 2 === 0) ? "O" : "X";
+    turnCount++
+}
+
+squares.forEach((square, index) => {
+    square.setAttribute('id', `${index}`)
+    square.addEventListener('click', handleClickEventCallback, {once: true})
 })
+
+//  let squares = document.querySelectorAll('#gridItemContainer>div')
+// // square1.addEventListener('click',(event) =>{
+// //
+//    console.log(squares)
+// squares.forEach( square=>
+// {
+//     square.addEventListener('click', (event)=>{
+//         event.target.setAttribute('style', 'background-color:red')
+//         console.log('its been pressed',event.target)
+//     })
+// })
 //     }
 // )
 
